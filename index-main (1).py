455316ffb329282f3dbe85e5613c05eafedfe75d
MahmoudMobarak <mahmoudmobarak28.11@gmail.com>
2025-04-15 19:54:39 +0400

    Update index-main (1).py

--- index-main (1).py
+++ index-main (1).py
@@ -9,7 +9,7 @@ from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 LR=LinearRegression()
 data=pd.read_csv('london_houses_cleaned.csv')
-x=data.drop['Address','Neighborhood,'Price (£)',axis=0]
+x=data.drop[r'Address',r'Neighborhood,r'Price (£)',axis=0]
 y=data['Price (£)']
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
 LR.fit(x_train,y_train)
